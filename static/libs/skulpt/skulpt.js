e70fa02a0046999c789aa8c7e817c21113180660
Austin Cory Bart <acbart@vt.edu>
2019-09-12 18:40:49 -0400

    Updated pedal defs

--- static/libs/skulpt/skulpt.js
+++ static/libs/skulpt/skulpt.js
@@ -35079,7 +35079,7 @@ var Sk = {}; // jshint ignore:line
 
 Sk.build = {
     githash: "b358af4824d08ce74dc6dd9bfffe9df9d619f806",
-    date: "2019-09-12T21:24:04.302Z"
+    date: "2019-09-12T22:38:21.227Z"
 };
 
 /**
