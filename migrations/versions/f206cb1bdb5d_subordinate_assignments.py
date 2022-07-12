217a5c14552f61bf067cfb0c840012c2086365f0
Austin Cory Bart <acbart@vt.edu>
2022-07-12 08:50:14 -0400

    New migration with subordinate tag for assignments

---
+++ migrations/versions/f206cb1bdb5d_subordinate_assignments.py
@@ -0,0 +1,24 @@
+"""Subordinate assignments
+
+Revision ID: f206cb1bdb5d
+Revises: be70df7520d1
+Create Date: 2022-06-28 12:12:43.636031
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f206cb1bdb5d'
+down_revision = 'be70df7520d1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('assignment', sa.Column('subordinate', sa.Boolean(), default=False))
+
+
+def downgrade():
+    op.drop_column('assignment', 'subordinate')
