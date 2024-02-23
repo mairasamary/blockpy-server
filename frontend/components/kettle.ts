e1ab8d4b68a1cc6030f6271ebab9657893241557
Austin Cory Bart <acbart@vt.edu>
2024-02-23 06:55:34 -0500

    Fix deep equality check

--- frontend/components/kettle.ts
+++ frontend/components/kettle.ts
@@ -251,30 +251,9 @@ addEventListener('message', (message) => {
 const EXECUTION_INSTRUCTOR = `// Instructor Header
 (function() {
 const isDeepEqual = (object1, object2) => {
-
-  const objKeys1 = Object.keys(object1);
-  const objKeys2 = Object.keys(object2);
-
-  if (objKeys1.length !== objKeys2.length) return false;
-
-  for (var key of objKeys1) {
-    const value1 = object1[key];
-    const value2 = object2[key];
-
-    const isObjects = isObject(value1) && isObject(value2);
-
-    if ((isObjects && !isDeepEqual(value1, value2)) ||
-      (!isObjects && value1 !== value2)
-    ) {
-      return false;
-    }
-  }
-  return true;
+    return JSON.stringify(object1) === JSON.stringify(object2);
 };
 
-const isObject = (object) => {
-  return object != null && typeof object === "object";
-};
 
 const DEFAULT_SUITE = "__GLOBAL";
 let currentSuite = DEFAULT_SUITE, currentTest = null;
