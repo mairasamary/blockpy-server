34370aa63b6249f658d3fb653bdd2ce6dc55d3d9
Austin Cory Bart <acbart@vt.edu>
2024-03-30 19:37:32 -0400

    Update skulpt

--- static/libs/skulpt/skulpt.js
+++ static/libs/skulpt/skulpt.js
@@ -37184,7 +37184,7 @@ var Sk = {}; // jshint ignore:line
 
 Sk.build = {
     githash: "7a6b8b5042a767a2f94b6908811c386e71b06541",
-    date: "2024-02-11T21:15:47.159Z"
+    date: "2024-03-30T23:01:25.354Z"
 };
 
 /**
