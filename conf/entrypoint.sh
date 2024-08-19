f0c5dbec41949efb9d46e052ef084354a355ead5
Austin Cory Bart <acbart@vt.edu>
2024-08-19 15:47:25 -0400

    Can we cd into the folder?

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -38,6 +38,8 @@ touch /usr/src/app/logs/uwsgi_blockpy.log
 # chown www-data:www-data /usr/src/app/logs/*.log
 # chmod 664 /usr/src/app/logs/*.log
 
+cd /usr/src/app
+
 python /usr/src/app/manage.py create_db
 python /usr/src/app/manage.py db upgrade
 python /usr/src/app/manage.py populate_db
