d652e7fc27f0cb02ee8a2600a2127a6034ac793d
Austin Cory Bart <acbart@vt.edu>
2024-08-21 11:19:57 -0400

    Delete old code

--- manage.py
+++ manage.py
@@ -23,40 +23,5 @@ def run_huey():
     consumer.run()
 
 
-
-"""
-# TODO: Handle Migrate stuff
-# TOOD: Handle all other commands conversion
-manager = Manager(app)
-
-# Server commands context
-manager.add_command("secure", Server(ssl_crt=secrets.get("LOCAL_SSL_CERT"),
-                                     ssl_key=secrets.get("LOCAL_SSL_KEY"),
-                                     threaded=True))
-manager.add_command("insecure", Server())
-
-# Database Commands
-manager.add_command("reset_db", ResetDB())
-manager.add_command("create_db", CreateDB())
-manager.add_command("populate_db", PopulateDB())
-manager.add_command("add_test_users_db", AddTestUsersDB())
-manager.add_command("add_maze_course_db", AddMazeCourse())
-manager.add_command("display_db", DisplayDB())
-manager.add_command("export_course", ExportCourse())
-manager.add_command("import_course", ImportCourse())
-manager.add_command("remove_course", RemoveCourse())
-manager.add_command("dump_db", DumpDB())
-manager.add_command("export_progsnap2", ExportProgSnap())
-
-# External commands
-manager.add_command("update_blockpy", UpdateBlockPy())
-
-# Migrate commands
-manager.add_command("db", MigrateCommand)
-
-# Task Commands
-manager.add_command("huey", HueyWorker())
-"""
-
 if __name__ == '__main__':
     cli()
