9e520d7ef4497148e3fe6f3d6cac42a592c688a1
Austin Cory Bart <acbart@vt.edu>
2024-08-21 12:39:05 -0400

    Debug info needed for redis

--- tasks/setup.py
+++ tasks/setup.py
@@ -8,6 +8,7 @@ def setup_tasks(task_queue_style, task_db_uri, task_db_settings):
     if task_queue_style == 'sqlite':
         huey = SqliteHuey(filename=task_db_uri)
     elif task_queue_style == 'redis':
+        print("Connecting to Redis", task_db_uri, task_db_settings)
         huey = RedisHuey(url=task_db_uri, **task_db_settings)
     else:
         raise ValueError(f"Unknown TASK_QUEUE_STYLE: {task_queue_style} (should be sqlite or redis)")
